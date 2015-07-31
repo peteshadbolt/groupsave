d3e7220c018248dcb9d88c737d5f34a20584a352
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-07-31 16:26:44 +0100

    Feels good man

--- test_api.py
+++ test_api.py
@@ -2,28 +2,23 @@ import tempfile
 from flask import session
 import groupsavr
 import unittest
+import database
 import os
 
 class APITestCase(unittest.TestCase):
     def setUp(self):
         #db_fd = tempfile.mkstemp()
         #self.db_fd, groupsavr.app.config['DATABASE'] = db_fd
-        groupsavr.app.config['TESTING'] = True
-        self.client = groupsavr.app.test_client()
-        self.app = groupsavr.app
-        #groupsavr.init_db()
+        app = groupsavr.get_app()
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+        self.app = app
+        database.init_db()
 
     def tearDown(self):
-        #os.close(self.db_fd)
-        #os.unlink(groupsavr.app.config['DATABASE'])
         pass
 
     def test_database_setup(self):
-        #con = groupsavr.connect_db()
-        #cur = con.execute('PRAGMA table_info(entries);')
-        #rows = cur.fetchall()
-        #print rows
-        #self.assertEquals(len(rows), 3)
         pass
 
 if __name__ == '__main__':
