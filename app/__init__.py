e7615b16c3f853c9a1b332ce46f0e41db7506a4d
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-08-02 23:20:54 +0100

    Tidy up

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('app.config')
+app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
 from app import api
-
