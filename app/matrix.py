c1aad353f318596bb4a5910fa2adfa818a4a854f
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-08-06 11:19:23 +0100

    Thinking about DB design

--- app/matrix.py
+++ app/matrix.py
@@ -1,14 +1,26 @@
 from redis import StrictRedis
 import csv
 
+def station_key(sid):
+    return "station:{:}".format(sid)
+
+
+def journey_key(sid1, sid2):
+    return "journey:{:}:{:}".format(sid1, sid2)
+
+
+def journeys_key(sid):
+    return "journeys:{:}".format(sid1)
+
+
 def populate():
     """ Pre-populates Redis with a list of stations """
     with open("raw_data/RailReferences.csv") as f:
         for row in csv.reader(f):
-            key = row[2].lower()
+            key = station_key(row[2].lower())
             name = row[3].replace("Rail Station", "")
             name = name.strip()
-            redis.set(key, name)
+            redis.hmset(key, station)
 
 redis = StrictRedis()
 if redis.dbsize()==0:
