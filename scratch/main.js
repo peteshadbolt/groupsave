1536998a32d8e4db98e1c8940e9acadd316f393f
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-08-28 13:13:53 -0700

    Autocomplete is good now, London a bit broken tho

--- scratch/main.js
+++ scratch/main.js
@@ -1,65 +1,51 @@
-document.addEventListener("DOMContentLoaded", setup);
+var from, to, showfrom, showto;
 
-// TODO: just sort that station list in terms of frequency of use haha
 function matchStation(s) {
     var output = [];    
-    if (s.length==0) return output;
+    if (s.length===0) return output;
     for ( var i = 0; i < stations.length; i++) {
         var station = stations[i];
-        if (station.name.toLowerCase().indexOf(s) != -1 || station.crs == s) {
-           output.push(station.name);
+        if (station[1].toLowerCase().indexOf(s) != -1 || station[0] == s) {
+           output.push(station);
         }
         if (output.length>5){return output;}
     }
     return output;
 }
 
-function updateFrom(args) {
-    var search = this.value.trim().toLowerCase();
-    var s = matchStation(search).join(" / ");
+function autocomplete(field, output) {
+    var stationAsLink = function (station){
+        var a = document.createElement('a');
+        var linkText = document.createTextNode(station[1]);
+        a.appendChild(linkText);
+        a.href = "#";
+        a.onclick = function () { field.value = station[1]; };
+        return a;
+    };
 
-    //if (search.length<3) {
-        //document.getElementById("showfrom").innerHTML="";
-        //return;
-    //}
-    //var result = fuse.search(search);
-    //var names = [];
-    //for (var i=0; i < result.length; ++i) {
-        //names.push(result[i].name);
-    //}
-    //var s = names.join(" / ");
-    document.getElementById("showfrom").innerHTML = s;
-}
+    var autocompleteListener = function(args) {
+        var search = field.value.trim().toLowerCase();
+        var matches = matchStation(search);
+        var links = matches.map(stationAsLink);
+        output.innerHTML = "";
+        if (links[0]) links[0].className="hi";
+        for (var i=0; i < links.length; ++i) {
+            output.appendChild(links[i]);
+            output.appendChild(document.createTextNode(" "));
+        }
+        if (links.length>5) {output.appendChild(document.createTextNode("..."));}
+    };
 
-function updateTo(args) {
-    var search = this.value.trim();
-    if (search.length<3) {
-        document.getElementById("showto").innerHTML = "";
-        return;
-    }
-    var result = fuse.search(search);
-    var names = [];
-    for (var i=0; i < result.length; ++i) {
-        names.push(result[i].name);
-    }
-    var s = names.join(" / ");
-    document.getElementById("showto").innerHTML = s;
-}
-
-function updateWhen(args) {
-    var search = this.value.trim();
-    var result = Date.parse(search);
-    var s = result;
-    document.getElementById("showwhen").innerHTML = s;
+    field.addEventListener("input", autocompleteListener);
 }
 
 function setup(argument) {
-    // Initiate fuse
-    //fuse = new Fuse(stations, fuseOptions); 
-
-    // Bind events
-    document.getElementById('from').addEventListener("input", updateFrom);
-    //document.getElementById('to').addEventListener("input", updateTo);
-    //document.getElementById('when').addEventListener("input", updateWhen);
+    from = document.getElementById("from");
+    to = document.getElementById("to");
+    showfrom = document.getElementById("showfrom");
+    showto = document.getElementById("showto");
+    autocomplete(from, showfrom);
+    autocomplete(to, showto);
 }
 
+document.addEventListener("DOMContentLoaded", setup);
