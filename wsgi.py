0cfd0e9a2655b554a5a500eb957d9daeeb12870e
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-09-02 14:09:54 -0700

    Welcome to Erf

--- wsgi.py
+++ wsgi.py
@@ -28,7 +28,7 @@ def application(environ, start_response):
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Welcome to OpenShift</title>
+  <title>Welcome to Erf</title>
 <style>
 
 /*!
