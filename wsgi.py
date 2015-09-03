b693e91a2e9ba4879b8be61c388da7ae0e4ba8ff
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-09-03 14:16:01 -0700

    Readme

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python
-import os
-
-#virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-#virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-#try:
-    #execfile(virtualenv, dict(__file__=virtualenv))
-#except IOError:
-    #pass
-
 from gs import app as application
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
-    # Serve forever
     httpd.serve_forever()
